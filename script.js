b5244652a46a70857d424c181896b309c608ae3d
jpbrglp <brg.joap@gmail.com>
2024-12-13 13:04:49 -0300

    Soma js

---
+++ script.js
@@ -0,0 +1,7 @@
+function enviar(){
+    const v1 = window.document.getElementById('n1');
+    const i1 = Number(v1.value);
+    const v2 = window.document.getElementById('n2');
+    const i2 = Number(v2.value);
+    const resultado = window.alert(i1 + i2)
+}
